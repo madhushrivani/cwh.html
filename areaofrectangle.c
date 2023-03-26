d5624dc24e777868aec7e2520d629b5abe760571
madhushrivani <madhushrivani@gmail.com>
2023-03-26 23:49:58 +0530

    area of rectangle

---
+++ areaofrectangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int l,b,AOR;
+    printf("Enter the length : ");
+    scanf("%d", &l);
+    printf("Enter the bredth : ");
+    scanf("%d", &b);
+    AOR = l*b;
+    printf("Area of the rectangle is %d", AOR);
+    getch ();
+}
