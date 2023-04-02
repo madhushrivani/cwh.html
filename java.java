9fd67bf1849b6f4499d20a13aba5788a473d2eaa
madhushrivani <madhushrivani@gmail.com>
2023-04-02 23:12:50 +0530

    my kat

---
+++ java.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class kat {
+  public static void main(String[] args) {
+    
+   int n=5;
+   for(int i=1,j=100; i<=n && j<=200; j+=20){
+    System.out.println(i+ " " +j);
+   }
+    
+    
+ }
+        
+    }
